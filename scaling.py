69fc7b004f0a15e449ae123b1abf4e2d529202ae
jhfmoore <jhfmooore@gmail.com>
2025-10-21 12:22:15 +0100

    Invariants now constant. Must consider if inputing/calculating correct parameters

--- scaling.py
+++ scaling.py
@@ -5,19 +5,6 @@ import numpy as np
 # Seeking to
 
 
-def verify_dimensionless(astro, lab):
-    u_a, r_a, t_a, n_a, p_a, B_a, eps_a = astro
-    u_l, r_l, t_l, n_l, p_l, B_l, eps_l = lab
-
-    equal = True
-
-    # Verify Reynolds number
-    #if / (u_a * ):
-        #equal = False
-
-
-    return equal
-
 # Set astrophysical values
 u_astro = 4e7               # SW speed (1AU)
 l_astro = 6.278e6           # Earth radius
@@ -44,7 +31,29 @@ T_lab = 1.16e4
 
 lab = np.array([u_lab, l_lab, t_lab, rho_lab, n_lab, p_ram_lab, B_lab, T_lab])
 
-def find_time_scale(astro, lab):
+
+# Define calculations
+
+def nerrst(u, l, T, tau, delta, hall, beta_1, beta_0, m):
+    return (((u * l * m)) / (T * tau)) * (delta / (beta_1 * hall**2 + beta_0))
+
+def biermann(rho, l, z, m, q, mu):
+    return (q * np.sqrt(mu * rho) * l * (1+z)) / m
+
+def delta(hall, delta_0, delta_1):
+    return hall ** 4 + delta_1 * hall ** 2 + delta_0
+
+def tau(T, n):
+    return 1.2e4 * T**(3/2) / n
+
+def omega(q, b, m):
+    return q * b / m
+
+def hall(tau, omega):
+    return tau * omega
+
+
+def find_scaling(astro, lab):
     u_a, l_a, t_a, rho_a, n_a, p_a, b_a, T_a = astro
     u_l, l_l, t_l, rho_l, n_l, p_l, b_l, T_l = lab
 
@@ -61,42 +70,54 @@ def find_time_scale(astro, lab):
     z = 1
 
     # Calculate values for SW
-    tau_a = 1.2e4 * T_a**(3/2) / n_a
-    omega_a = q_e * b_a / m_e
-    hall_a = tau_a * omega_a
-    delta_a = hall_a**4 + delta_1 * hall_a**2 + delta_0
+    tau_a = tau(T_a, n_a)
+    omega_a = omega(q_e, b_a, m_e)
+    hall_a = hall(tau_a, omega_a)
+    delta_a = delta(hall_a, delta_0, delta_1)
 
-    # Calculate the dimensionless numbers (Nerst and Bierrmann) constant across scaling, for the SW
-    nerrst = ((u_a * l_a * m_e) / (T_a * tau_a)) * (delta_a / (beta_1 * hall_a**2 +beta_0))
-    biermann = (q_e * np.sqrt(mu * rho_a) * l_a * (1 + z)) / m_e
+    # Calculate the dimensionless numbers (Nerrst and Bierrmann) constant across scaling, for the SW
+    nerrst_a = nerrst(u_a, l_a, T_a, tau_a, delta_a, hall_a, beta_1, beta_0, m_e)
+    biermann_a = biermann(rho_a, l_a, z, m_e, q_e, mu)
 
-    rho_l = (m_e * biermann) / (q_e * l_l * (1 + z)) ** 2 / mu
+    rho_l = ((m_e * biermann_a) / (q_e * l_l * (1 + z))) ** 2 / mu
 
     # Calculate B_lab
-    tau_l = 1.2e4 * T_l**(3/2) / n_l
-    rhs =  (u_l * l_l * m_e) / (nerrst * T_l * tau_l)
-    omega_l = q_e * b_l / m_e
-    hall_l = tau_l * omega_l
-    delta_l = hall_l ** 4 + delta_1 * hall_a ** 2 + delta_0
+    tau_l = tau(T_l, n_l)
+    rhs =  (u_l * l_l * m_e) / (nerrst_a * T_l * tau_l)
+    omega_l = omega(q_e, b_l, m_e)
 
     hall_l = np.roots([rhs,
                         0,
-                        delta_1 * rhs - beta_0,
-                        delta_0 * rhs - beta_0])[0]
+                        delta_1 * rhs - beta_1,
+                        0,
+                        delta_0 * rhs - beta_0])[1]
+
+    delta_l = delta(hall_l, delta_0, delta_1)
 
     b_l = (hall_l * m_e) / (q_e * tau_l)
 
     print("Lab mass density: " + str(rho_l))
     print("Lab B: " + str(b_l))
 
-    print("Nerrst astro: " + str(nerrst))
-    print("Biermann astro: " + str(biermann))
+    print("Nerrst astro: " + str(nerrst_a))
+    print("Biermann astro: " + str(biermann_a))
 
-    nerrst_lab = ((u_l * l_l * m_e) / (T_l * tau_l)) * (delta_l / (beta_1 * hall_l**2 + beta_0))
-    biermann_lab = (q_e * np.sqrt(mu * rho_l) * l_l * (1 + z)) / m_e
+    nerrst_l = nerrst(u_l, l_l, T_l, tau_l, delta_l, hall_l, beta_1, beta_0, m_e)
+    biermann_l = biermann(rho_l, l_l, z, m_e, q_e, mu)
 
-    print("Nerrst lab: " + str(nerrst_lab))
-    print("Biermann lab: " + str(biermann_lab))
+    print("Nerrst lab: " + str(nerrst_l))
+    print("Biermann lab: " + str(biermann_l))
 
 
-find_time_scale(astro, lab)
+    lab[2] = lab[1] / lab[0]
+    lab[3] = rho_l
+    lab[6] = b_l
+
+    return lab
+
+
+lab = find_scaling(astro, lab)
+
+print()
+print(astro)
+print(lab)
